4c3071e9269f05cef5f65942f9824bddb173e0ee
aracely-huamani <102335298+aracely-huamani@users.noreply.github.com>
2023-06-10 18:07:00 -0500

    Agregar archivos de proyecto.

---
+++ lab11/lab11/Views/Operacions.xaml.cs
@@ -0,0 +1,22 @@
+﻿using lab11.Viewmodels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace lab11.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Operacions : ContentPage
+    {
+        public Operacions()
+        {
+            InitializeComponent();
+            this.BindingContext = new ViewModelOperaciones();
+        }
+    }
+}
